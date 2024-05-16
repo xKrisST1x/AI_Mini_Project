ae5242835bc890ddb30d990da49973c853f394ae
Kristoffer Troelsen <kristroelsen@hotmail.com>
2024-05-16 19:42:25 +0200

    3

--- Assets/Scripts/BehaviourTree/Sequence.cs
+++ Assets/Scripts/BehaviourTree/Sequence.cs
@@ -2,17 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sequence : MonoBehaviour
+namespace BehaviourTree
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Sequence : Node
     {
-        
-    }
+        public Sequence() : base() { }
+        public Sequence(List<Node> children) : base(children) { }
+        public override NodeState Evaluate()
+        {
+            bool anyChildIsRunning = false;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            foreach(Node node in children)
+            {
+                switch(node.Evaluate())
+                {
+                    case NodeState.FAILURE:
+                        state = NodeState.FAILURE;
+                        return state;
+                    case NodeState.SUCCESS:
+                        continue;
+                    case NodeState.RUNNING:
+                        anyChildIsRunning = true;
+                        continue;
+                    default:
+                        state = NodeState.SUCCESS;
+                        return state;
+                }
+            }
+
+            state = anyChildIsRunning ? NodeState.RUNNING : NodeState.SUCCESS;
+            return state;
+        }
     }
 }
---
+++ Assets/Scripts/GuardAI/GuardBT.cs
@@ -0,0 +1,7 @@
+using BehaviourTree;
+
+public class GuardBT : Tree1
+{
+    // Kommet til 14:40 https://www.youtube.com/watch?v=aR6wt5BlE-E&ab_channel=MinaP%C3%AAcheux
+
+}
